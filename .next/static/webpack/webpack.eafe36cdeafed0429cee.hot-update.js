13bf68d8fac5cce60ccd0ebc417c3a94181edaba
NwaforAugustine321 <nwaforaugustine422@gmail.com>
2022-05-25 23:21:30 +0100

    update2

--- .next/static/webpack/webpack.eafe36cdeafed0429cee.hot-update.js
+++ .next/static/webpack/webpack.eafe36cdeafed0429cee.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8bf93a9c475acd0031d7"; }
+/******/ 	__webpack_require__.h = function() { return "3acc7932ad77482212ba"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
